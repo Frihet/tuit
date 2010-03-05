537c9d5a38fa8daec1b9f7bffa0fa7d65a7ab8d0
Axel Lingoncvantz <axel.liljencrantz@freecode.no>
2010-03-05 08:12:48 +0000

    Use Norwegian help

--- help/views.py
+++ help/views.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import *
 from tuit.util import *
 #import re
 from django.utils.translation import gettext as _
+import tuit.settings 
 
 @login_required
 def help(request):
-    return tuit_render('help.html', {'title':_('Help')}, request)
-    
+    try:
+        return tuit_render("help.%s.html" % tuit.settings.LANGUAGE_CODE, {'title':_('Help')}, request)
+    except:
+        return tuit_render('help.html', {'title':_('Help')}, request)
+
