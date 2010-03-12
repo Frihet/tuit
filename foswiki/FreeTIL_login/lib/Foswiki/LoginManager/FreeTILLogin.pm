6dd5304c85dda14e6fe4091e74b51eaa148e2aa9
Axel Lingoncvantz <axel.liljencrantz@freecode.no>
2010-03-12 16:17:00 +0100

    Added foswiki user/group integration

---
+++ foswiki/FreeTIL_login/lib/Foswiki/LoginManager/FreeTILLogin.pm
@@ -0,0 +1,130 @@
+# Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2007-2009 Michael Daum http://michaeldaumconsulting.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+package Foswiki::LoginManager::FreeTILLogin;
+use base Foswiki::LoginManager::ApacheLogin;
+use strict;
+use warnings;
+use Assert;
+use Foswiki::LoginManager::ApacheLogin;
+use Foswiki::Sandbox;
+use WWW::Curl::Easy;
+use JSON;
+
+# @Foswiki::LoginManager::FreeTILLogin::ISA = qw( Foswiki::LoginManager::ApacheLogin );
+
+sub new {
+  my ($class, $session) = @_;
+
+  my $this = bless( $class->SUPER::new($session), $class );
+  $session->{loginManager} = $this;
+
+  Foswiki::registerTagHandler( 'LOGOUT',    \&_LOGOUT );
+  Foswiki::registerTagHandler( 'LOGOUTURL', \&_LOGOUTURL );
+
+  return $this;
+}
+
+
+sub _LOGOUTURL {
+    return '/tuit/account/logout';
+}
+
+sub _LOGOUT {
+    my ( $session, $params, $topic, $web ) = @_;
+    my $this = $session->{users}->{loginManager};
+
+    return '' unless $session->inContext('authenticated');
+
+    my $url = _LOGOUTURL(@_);
+    if ($url) {
+        my $text = $session->templates->expandTemplate('LOG_OUT');
+        return CGI::a( { href => $url }, $text );
+    }
+    return '';
+}
+
+
+sub forceAuthentication {
+    return 0;
+}
+
+sub loginUrl {
+    return '/tuit/account/login/';
+}
+
+sub getUser {
+    my $this = shift;
+
+    if (!$this->{_cgisession}) {
+	return undef;
+    }
+
+    my $session_id = $this->{session}->{request}->cookie('sessionid');
+    my $server_host = $ENV{'SERVER_NAME'};
+    my $browser_host = $ENV{'HTTP_HOST'};
+    my $request_uri = $ENV{'REQUEST_URI'};
+    my $server_port = $ENV{'SERVER_PORT'};
+    my $port_part = ($server_port != 80) ? ":$server_port" : "";
+
+    my $curl = new WWW::Curl::Easy;
+    $curl->setopt(CURLOPT_HEADER, 0);
+    $curl->setopt(CURLOPT_URL, "http://" . $server_host . $port_part . "/tuit/account/session/");
+    $curl->setopt(CURLOPT_FOLLOWLOCATION, 1);
+    $curl->setopt(CURLOPT_COOKIE, "sessionid=" . $session_id);
+    $curl->setopt(CURLOPT_NOPROGRESS, 1);
+
+    my $response_header;
+    open (my $fileh, ">", \$response_header);
+    $curl->setopt(CURLOPT_WRITEHEADER, \$fileh);
+
+    my $response_body;
+    open (my $fileb, ">", \$response_body);
+    $curl->setopt(CURLOPT_FILE, \$fileb);
+
+    # Starts the actual request
+    my $retcode = $curl->perform;
+
+    # Looking at the results...
+    if ($retcode == 0) {
+	    my $response_data = jsonToObj($response_body);
+
+	    if (!$response_data->{'username'}) {
+		$this->{session}->{response}
+		  ->redirect( -url => "/tuit/account/login/");
+                return;
+	    }
+
+	    $this->{_cgisession}->param('FREETIL_USER', $response_data->{username});
+	    $this->{_cgisession}->param('FREETIL_GROUPS', join(',', @{$response_data->{groups}}));
+
+	    return $response_data->{'username'};
+    } else {
+	    die("Unable to fetch auth data");
+		    Foswiki::LoginManager::_trace( $this,
+			"Unable to fetch login data from FreeTIL: " .$curl->strerror($retcode)." ($retcode)\n");
+	    $this->{session}->{response}
+	      ->redirect( -url => "/tuit/account/login/");
+
+    }
+}
+
+sub loadSession {
+    my $this = shift;
+
+    my $authUser = $this->SUPER::loadSession(@_);
+    return $this->getUser();
+}
+
+1;
---
+++ foswiki/FreeTIL_login/lib/Foswiki/Users/FreeTILUserMapping.pm
@@ -0,0 +1,133 @@
+# Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2006-2009 Michael Daum http://michaeldaumconsulting.com
+# Portions Copyright (C) 2006 Spanlink Communications
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+
+package Foswiki::Users::FreeTILUserMapping;
+
+use strict;
+use base 'Foswiki::UserMapping'; #Foswiki::Users::TopicUserMapping';
+use Foswiki::ListIterator ();
+use vars qw($isLoadedMapping);
+
+sub new {
+  my ($class, $session) = @_;
+
+  my $this = bless($class->SUPER::new($session), $class);
+
+  @{$this->{eachGroupMember}} = split(/,/, $session->{loginManager}->{_cgisession}->param('FREETIL_GROUPS'));
+  $this->{authUser} = $session->{loginManager}->{_cgisession}->param('FREETIL_USER');
+  return $this;
+}
+
+sub addUser {
+  throw Exception("Please use the FreeTIL user administration tools, not the foswiki ones");
+}
+
+sub getLoginName {
+  my ($this, $cUID) = @_;
+  print "\n\n";
+  print $cUID;
+  die(1);
+  return $cUID;
+}
+
+sub getWikiName {
+  my ($this, $cUID) = @_;
+  print "\n\n";
+  print $cUID;
+  die(1);
+  return $cUID;
+}
+
+sub getEmails {
+  return ();
+}
+
+sub userExists {
+  return 1;
+}
+
+sub eachUser {
+  return new Foswiki::ListIterator(());
+}
+
+sub findUserByEmail {
+  throw Exception("Not implemented");
+}
+
+sub findUserByWikiName {
+  my ($this, $wikiName) = @_;
+  print "\n\n";
+  print $wikiName;
+  die(1);
+  return ($wikiName);
+}
+
+sub login2cUID {
+  my ($this, $name, $dontcheck) = @_;
+  print "\n\n";
+  print $name;
+  die(1);
+  return $name;
+}
+
+sub eachGroupMember {
+  my ($this, $groupName, $seen) = @_;
+
+  # print "\n\n";
+  # print "GROUP: ", $groupName, "\n";
+  # print "GROUPS: ", join(", ", @{$this->{eachGroupMember}}), "\n";
+  # print "\n\n";
+
+  if (grep { "$_" eq "$groupName" } @{$this->{eachGroupMember}}) {
+    return new Foswiki::ListIterator([$this->{authUser}]);
+  }
+  return new Foswiki::ListIterator([]);
+}
+
+sub isGroup {
+    my ($this, $groupName) = @_;
+    return grep { "$_" eq "$groupName" } @{$this->{eachGroupMember}};
+}
+
+sub eachGroup {
+    my ($this) = @_;
+    return new Foswiki::ListIterator(@{$this->{eachGroupMember}});
+}
+
+
+
+
+sub eachMembership {
+  my ($this, $cUID) = @_;
+
+  print "\n\n";
+  print "GROUPS: ", $this->{loginManager}->{_cgisession}->param('FREETIL_GROUPS');
+  print "\n\n";
+  die(1);
+
+  my @groups = $this->getListOfGroups();
+
+  my $it = new Foswiki::ListIterator( \@groups );
+  $it->{filter} = sub {
+    $this->isInGroup($cUID, $_[0]);
+  };
+
+  return $it;
+}
+
+
+1;
