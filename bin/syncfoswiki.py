df9849f5e26611de04ac25553b7a3f436b9291d5
Axel Lingoncvantz <axel.liljencrantz@freecode.no>
2011-03-04 11:17:27 +0100

    Workoaround script for handling foswiki groups manually

---
+++ bin/syncfoswiki.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+import os, os.path, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+os.environ["DJANGO_SETTINGS_MODULE"] = 'tuit.settings'
+
+import django.core.management
+import django.db.transaction
+
+import tuit.mail
+import logging
+import traceback
+import re
+from django.contrib.auth.models import *
+
+admgrp = Group.objects.filter(name='AdminGroup')
+for g in admgrp:
+    val = ",".join(map(lambda x:x.username, g.user_set.all()))
+    with open("/var/lib/foswiki/data/Main/AdminGroup.txt") as f:
+        content = f.read()
+
+        content = re.sub(r'\* *Set *GROUP *=.*',
+               r'* Set GROUP =' + val,
+               content)
+
+    with open("/var/lib/foswiki/data/Main/AdminGroup.txt","w") as f:
+        f.write(content)
+
+
+logging.getLogger('foswiki').info('Finished foswiki sync')
